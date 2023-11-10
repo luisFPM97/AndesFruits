3ac99dd16903f2ce050bc1c104201b0f35c244f5
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2023-11-10 11:30:13 -0500

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       <div className="subProducts">
         <div className="products">
           <div className="product">
-            <img className='imgProduct' src='/public/img/uchuva.png' alt="imgProduct" />{" "}
+            <img className='imgProduct' src='https://cdn.portalfruticola.com/2022/11/c92944e8-3712-4369-a9f3-10f689db5e0a.jpg' alt="imgProduct" />{" "}
             <a className="productName" href="">
               UCHUVA
             </a>
