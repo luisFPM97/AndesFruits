f0049011d8540b7ed8bf70937340f4ed989a62f2
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-05-27 08:19:39 -0500

    update btn menu

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = ({setLanguage, language}) => {
   return (
     
     <div className={showNav ? 'mainM responsive' : 'mainM'}>
-      <Link  to="/" className={showNav ? 'lA lAr' : 'lA'} ><img  onClick={() => setShowNav(true)} src="/img/Recurso.png" alt="logo" /></Link>
-      
+      <Link  to="/" className={showNav ? 'lA lAr' : 'lA'} ><img   src="/img/Recurso.png" alt="logo" /></Link>
+      <button className={showNav ? 'bntMenu btnMr' : 'bntMenu'} onClick={() => setShowNav(true)}><i class='bx bx-menu'></i><span>Menú</span></button>
       <Link className={showNav ? 'logoA' : 'logoA'} to="/"  ><img onClick={() => setShowNav(true)} className='logo' src="/img/Recurso.png" alt="logo" /></Link>
       <a className='icon'   onClick={() => setShowNav(!showNav)}>{showNav ? 'X' : ''}</a>
       <div className='language' onClick={changeLanguage}>
