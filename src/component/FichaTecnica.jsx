2e3d2bbde34293dec2bfa05a81245145dc373fe9
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-05-28 17:27:03 -0500

    update data ft

--- src/component/FichaTecnica.jsx
+++ src/component/FichaTecnica.jsx
@@ -6,8 +6,24 @@ const FichaTecnica = ({productos,productid}) => {
     console.log(product)
   return (
     <div>
-        <h1>{product.title}</h1>
+        <h1>{product.ftitle}</h1>
+        <img src={product.image} alt="" />
+        <h2>{product.title}</h2>
+        <h3>{product.scientificName}</h3>
         <span>{product.phrase}</span>
+        <span>{product.curiousFact}</span>
+        <span>{product.description}</span>
+        <div>
+          {
+            product.table.map((data,i)=>(
+              
+              <div key={i}>
+                <span>{data.title} </span><span>{data.value} </span><span>{data.unit} </span>
+              </div>
+            ))
+          }
+        </div>
+
     </div>
   )
 }
