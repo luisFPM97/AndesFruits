a989d0d47ebbbd31f360c6414ab33c5680059f74
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-05-30 16:46:50 -0500

    update ficha tecnica

--- src/component/FichaTecnica.jsx
+++ src/component/FichaTecnica.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const FichaTecnica = ({productos,productid}) => {
     
@@ -6,6 +7,7 @@ const FichaTecnica = ({productos,productid}) => {
 
   return (
     <div className='fichaTecnica'>
+        <div className='rute'><Link className='linkP' to='/productos'>Productos</Link> <span>/</span><span>{product.title}</span></div>
         <h1>{product.ftitle}</h1>
         <div className='ftTitle'>
           <h2>{product.title}</h2>
@@ -13,39 +15,60 @@ const FichaTecnica = ({productos,productid}) => {
         </div>
         <div className='content'>
 
-        <div className='info'>
-            <span>{product.phrase}</span>
-            <span>{product.curiousFact}</span>
-            
-        </div>
+          <div className='info'>
+              <span>{product.phrase}</span>
+              <span>{product.curiousFact}</span>
+              
+          </div>
 
-        <div className='img'>
-            <img src={product.image} alt="" />
-        </div>
+          <div className='img'>
+              <img src={product.image} alt="" />
+          </div>
     
-        <div className='table'>
-          <table>
-              <tr>
-                <th>Element</th>
-                <th>Unit</th>
-                <th>Total</th>
-              </tr>
-              {
-                product.table.map((data,i)=>(
-                  
-                  <tr key={i}>
-                    <td className='aleft'>{data.title} </td>
-                    <td className='aright'>{data.unit} </td>
-                    <td className='aright'>{data.value} </td>
-                  </tr>
-                ))
-              }
-          </table>
-        </div>
+          <div className='table'>
+            <table>
+                <tr>
+                  <th>Element</th>
+                  <th>Unit</th>
+                  <th>Total</th>
+                </tr>
+                {
+                  product.table.map((data,i)=>(
+                    
+                    <tr key={i}>
+                      <td className='aleft'>{data.title} </td>
+                      <td className='aright'>{data.unit} </td>
+                      <td className='aright'>{data.value} </td>
+                    </tr>
+                  ))
+                }
+            </table>
+          </div>
 
         </div>
         <div className='description'>
           <span >{product.description}</span>
+          <br />
+          <br />
+          <br />
+          <br />
+          <span>{product.t1}</span>
+          <span>{product.d1}</span>
+          <br />
+          <span>{product.t2}</span>
+          <span>{product.d2}</span>
+          <br />
+          <span>{product.t3}</span>
+          <span>{product.d3}</span>
+          <br />
+          <span>{product.t4}</span>
+          <span>{product.d4}</span>
+          <br />
+          <span>{product.t5}</span>
+          <span>{product.d5}</span>
+          <br />
+          <span>{product.t6}</span>
+          <br />
         </div>
 
         
