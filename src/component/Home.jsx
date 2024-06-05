84e67fc608c64af3e642a659866a17052429c7c7
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-06-05 11:57:14 -0500

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -27,8 +27,7 @@ const Home = ({home, productos,setProductid,productid}) => {
           <div className="filter"></div>
         </section>
         <section className="homeProduct">
-        <img className="arrowSlider aleft" src="/img/arrow.svg" alt="" />
-        <img className="arrowSlider aright" src="/img/arrow.svg" alt="Imagen de google" />
+        
           <div className="hpTitle">
             <span>{home.sections[1].description}</span>
           </div>
