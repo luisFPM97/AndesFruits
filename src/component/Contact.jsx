fa80fba0174ef9d31a95486b21928a1d112d3789
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-06-12 08:23:25 -0500

    Update Contact.jsx

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,18 +1,12 @@
-import ipify from 'ipify';
+
 import React, { useEffect, useState } from 'react'
 
 
 const Contact = () => {
-  const [ip, setIp] = useState('');
-
-  useEffect(() => {
-    ipify.get().then((data) => {
-      setIp(data.ip);
-    });
-  }, []);
+  
   return (
     <div>
-      {ip && <p>Tu dirección IP: {ip}</p>}
+      
     </div>
   )
 }
